1afe20c2b7a38a0a3f941ea62c8349dae4ca995a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-04-18 05:24:02 +0200

    Add menu on the right

--- src/submit/submit.jsx
+++ src/submit/submit.jsx
@@ -131,7 +131,7 @@ const SubmitRouter = React.createClass({
   render() {
     return (
       <div>
-      <TitleBar>
+      <TitleBar active='Add a project' >
         <div className='titleText'>
           {'Submit a project'}
         </div>
